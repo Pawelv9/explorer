b44929b630d7d03da378fe8be30bf0a2ca2bf588
Pawelv9 <Pawelv9@gmail.com>
2026-05-21 23:00:37 +0300

    creating reusable components

---
+++ src/app/dashboard/dashboard-item/dashboard-item.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.html',
+  styleUrl: './dashboard-item.css',
+})
+export class DashboardItem {}

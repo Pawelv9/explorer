d22bcc8cd8d73f819fdd8160b2ca5a097bdd3632
Pawelv9 <Pawelv9@gmail.com>
2026-06-01 23:55:29 +0300

    accessing Host Elements programmatically

--- src/app/shared/control/control.ts
+++ src/app/shared/control/control.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,9 +8,20 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    '(click)': 'onClick()'
   }
 })
 export class Control {
+  // @HostBinding('class') className = 'control'
+  // @HostListener('click') onClick() {
+  //   console.log('clicked');
+  // }
   label = input.required<string>();
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log('clicked');
+    console.log(this.el);
+  }
 }

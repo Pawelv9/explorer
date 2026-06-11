5edd20af5e2c6a86904a31f788cc55f786f139c2
Pawelv9 <Pawelv9@gmail.com>
2026-06-11 23:44:23 +0300

    Signal Effects

--- src/app/shared/control/control.ts
+++ src/app/shared/control/control.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
+import { Component, contentChild, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -19,9 +19,12 @@ export class Control {
   // }
   label = input.required<string>();
   private el = inject(ElementRef);
+  // @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>;
+  private control = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input')
 
   onClick() {
     console.log('clicked');
     console.log(this.el);
+    console.log(this.control());
   }
 }
